ae7334b6c41676f6336cd4981cea45e6c7077574
evgeniy.isaev <isaevem@kamaz.ru>
2023-11-08 09:21:16 +0300

    first task v1

--- messages.py
+++ messages.py
@@ -1,6 +1,10 @@
 from enum import Enum
 
 class MessageType(Enum):
-    Greetings = 'Hi'
-    RussianGreetings = 'Привет'
-    Address = 'Address'
+    Address = 0
+    Distance = 1
+    Price = 2
+    PricePerKm = 2
+    Accept = 3
+    Ok = 4
+    
