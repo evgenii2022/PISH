a3282dfa89ca1e9cabaadd4c0fd824a997c8304c
evgeniy.isaev <isaevem@kamaz.ru>
2023-11-08 13:32:32 +0300

    first task: encapsulation

--- multiagent_system.py
+++ multiagent_system.py
@@ -2,13 +2,17 @@ from typing import Any
 from thespian.actors import *
 from messages import MessageType
 
-COURIER_PRICES = [13.5, 15.12, 9, 12, 11]
+ORDER_DISTANCE = 10
+COURIER_PRICES = [10, 15]
+
+# try uncommenting the line below
+# COURIER_PRICES = [13.5, 15.12, 9, 12, 11]
 
 class OrderActor(Actor):
     def __init__(self):
         super().__init__()
-        self.courier_prices = {}
-        self.distance = None
+        self.__courier_prices = {}
+        self.__distance = None
         
     def receiveMessage(self, msg: tuple[MessageType, Any], sender: ActorAddress):
         msg_type, msg_data = msg
@@ -16,23 +20,23 @@ class OrderActor(Actor):
         match msg_type:
             
             case MessageType.Address:
-                if self.distance is None:
+                if self.__distance is None:
                     raise Exception("My distance is None")
                 self.log(f"Send to {msg_data} my distance")
-                self.send(msg_data, (MessageType.Distance, self.distance))
+                self.send(msg_data, (MessageType.Distance, self.__distance))
                 
             case MessageType.Price:
-                self.courier_prices[sender.actorAddressString] = (msg_data, sender)
+                self.__courier_prices[sender.actorAddressString] = (msg_data, sender)
                 self.log(f"Get the price {msg_data} from {sender}")
                 
-                if len(self.courier_prices) == len(COURIER_PRICES):
+                if len(self.__courier_prices) == len(COURIER_PRICES):
                     chosen = self.choose_courier()
                     self.send(chosen, (MessageType.Accept, ''))
                     self.log(f"I choose {chosen}")
                     
             case MessageType.Distance:
-                self.distance = float(msg_data)
-                self.log(f"My distance is {self.distance}")
+                self.__distance = float(msg_data)
+                self.log(f"My distance is {self.__distance}")
                 
             case MessageType.Ok:
                 self.log(f"Courier {sender} must grab me")
@@ -41,7 +45,7 @@ class OrderActor(Actor):
                 raise Exception("Wrong message type: {_}")
     
     def choose_courier(self):
-        demanded_courier = min(self.courier_prices.items(), key=lambda p: p[1][0])
+        demanded_courier = min(self.__courier_prices.items(), key=lambda p: p[1][0])
         return demanded_courier[1][1]
     
     def log(self, data: str):
@@ -51,7 +55,7 @@ class OrderActor(Actor):
 class CourierActor(Actor): 
     def __init__(self):
         super().__init__()
-        self.price = None
+        self.__price = None
         
     def receiveMessage(self, msg: tuple[MessageType, Any], sender: ActorAddress):
         msg_type, msg_data = msg
@@ -59,9 +63,9 @@ class CourierActor(Actor):
         match msg_type:
             
             case MessageType.Distance:
-                if self.price is None:
+                if self.__price is None:
                     raise Exception("My price is None")
-                price = self.price * float(msg_data)
+                price = self.__price * float(msg_data)
                 self.log(f"I send my price for order: {price}")
                 self.send(sender, (MessageType.Price, price))
                 
@@ -70,8 +74,8 @@ class CourierActor(Actor):
                 self.send(sender, (MessageType.Ok, ''))
             
             case MessageType.PricePerKm:
-                self.price = float(msg_data)
-                self.log(f"My price per km is {self.price}")
+                self.__price = float(msg_data)
+                self.log(f"My price per km is {self.__price}")
                 
             case _:
                 raise Exception("Wrong message type: {_}")
@@ -84,7 +88,7 @@ def main():
     actor_system = ActorSystem()
     
     order = actor_system.createActor(OrderActor)
-    actor_system.tell(order, (MessageType.Distance, 10))
+    actor_system.tell(order, (MessageType.Distance, ORDER_DISTANCE))
     
     couriers = []
     for courier_price in COURIER_PRICES:
@@ -92,6 +96,8 @@ def main():
         couriers.append(courier)
         actor_system.tell(courier, (MessageType.PricePerKm, courier_price))
         actor_system.tell(order, (MessageType.Address, courier))
+    
+        
     
 if __name__ == "__main__":
     main()
